3c40b669ced0de41e1366d6316d48e43d9ae415f
ikromovvv <isardor859@gmail.com>
2024-10-23 14:06:56 +0300

    dasd

--- src/app/routers/ui/AppRouter.js
+++ src/app/routers/ui/AppRouter.js
@@ -1,26 +1,33 @@
+import "app/styles/index.sass"
 import classNames from "classnames";
-import {Layout} from "app/layout";
 import {Suspense} from 'react';
 import {createRoutesFromElements, Route, RouterProvider} from "react-router";
 import {createBrowserRouter} from "react-router-dom";
-import "app/styles/index.sass"
 
 import {routersConfig} from "../config/configRouter";
+import {Layout} from "../../layout/Layout";
+import {HomePage} from "../../../pages/homePage";
+import {Login} from "../../../pages/login";
 
 export const AppRouter = () => {
 
     const router = createBrowserRouter(
         createRoutesFromElements(
-            <Route element={<Layout/>}>
-                {
-                    routersConfig.map(item =>
+            <>
+
+                <Route path={"/"} element={<HomePage/>}/>
+                <Route path={"/login"} element={<Login/>}/>
+
+                <Route path={"platform/*"} element={<Layout/>}>
+                    {routersConfig.map(item =>
                         <Route
                             path={item.path}
                             element={item.element}
                         />
-                    )
-                }
-            </Route>
+                    )}
+                </Route>
+
+            </>
         )
     );
 
